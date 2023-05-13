f12697c51a25dcd42bb3b16aa40d02e7cb72a7c0
Daffa SH <shidqidaffa@gmail.com>
2023-05-13 11:43:01 +0700

    first init

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,18 @@
 // Import our custom CSS
-import "../scss/main.scss";
+import '../scss/main.scss';
 
-import App from "./views/app";
-import * as bootstrap from "bootstrap";
+import App from './views/app';
+import * as bootstrap from 'bootstrap';
 
 const app = new App({
-    content: document.getElementById("mainContent"),
-    navListContainer: document.getElementById("navListContainer"),
+  content: document.getElementById('mainContent'),
+  navListContainer: document.getElementById('navListContainer'),
 });
 
-window.addEventListener("hashchange", () => {
-    app.renderPage();
+window.addEventListener('hashchange', () => {
+  app.renderPage();
 });
 
-window.addEventListener("load", async () => {
-    app.renderPage();
+window.addEventListener('load', async () => {
+  app.renderPage();
 });
--- src/js/utils/navbar-initiator.js
+++ src/js/utils/navbar-initiator.js
@@ -1,35 +1,38 @@
-import AuthApi from "../networks/auth-api";
-import { authenticatedNavListTemplate, unauthenticatedNavListTemplate } from "../views/templates/template-creator";
+import AuthApi from '../networks/auth-api';
+import {
+  authenticatedNavListTemplate,
+  unauthenticatedNavListTemplate,
+} from '../views/templates/template-creator';
 
 const NavbarInitiator = {
-    async renderAuthenticatedNavList(navListContainer) {
-        try {
-            const response = await AuthApi.getUserInfo();
+  async renderAuthenticatedNavList(navListContainer) {
+    try {
+      const response = await AuthApi.getUserInfo();
 
-            navListContainer.innerHTML = authenticatedNavListTemplate(response.data);
-            this._initialUnauthListener();
-        } catch (error) {
-            console.log(error);
-        }
-    },
+      navListContainer.innerHTML = authenticatedNavListTemplate(response.data);
+      this._initialUnauthListener();
+    } catch (error) {
+      console.log(error);
+    }
+  },
 
-    _initialUnauthListener() {
-        const logoutButton = document.getElementById("userLogOut");
-        logoutButton.addEventListener("click", async (event) => {
-            event.preventDefault();
+  _initialUnauthListener() {
+    const logoutButton = document.getElementById('userLogOut');
+    logoutButton.addEventListener('click', async (event) => {
+      event.preventDefault();
 
-            try {
-                const response = await AuthApi.logout();
-                window.location.hash = "#/login";
-            } catch (error) {
-                console.error(error);
-            }
-        });
-    },
+      try {
+        const response = await AuthApi.logout();
+        window.location.hash = '#/login';
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  },
 
-    renderUnauthenticatedNavList(navListContainer) {
-        navListContainer.innerHTML = unauthenticatedNavListTemplate();
-    },
+  renderUnauthenticatedNavList(navListContainer) {
+    navListContainer.innerHTML = unauthenticatedNavListTemplate();
+  },
 };
 
 export default NavbarInitiator;
