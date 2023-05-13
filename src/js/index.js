cacee983d888b3e458fa10fb1c1535c79483dd99
Daffa SH <shidqidaffa@gmail.com>
2023-05-13 11:39:12 +0700

    first init

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+// Import our custom CSS
+import "../scss/main.scss";
+
+import App from "./views/app";
+import * as bootstrap from "bootstrap";
+
+const app = new App({
+    content: document.getElementById("mainContent"),
+    navListContainer: document.getElementById("navListContainer"),
+});
+
+window.addEventListener("hashchange", () => {
+    app.renderPage();
+});
+
+window.addEventListener("load", async () => {
+    app.renderPage();
+});
