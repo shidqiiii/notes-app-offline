1fd5cb6f2f4203922dc182509233f67e92a99270
Daffa SH <shidqidaffa@gmail.com>
2023-05-14 11:59:28 +0700

    Sesi 4 - Native Capabilities (Database dengan IndexedDB)

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,16 +1,18 @@
-import DashboardPage from './views/pages/dashboard';
-import CreateNotePage from './views/pages/notes/create';
-import LoginPage from './views/pages/auth/login';
-import RegisterPage from './views/pages/auth/register';
+import DashboardPage from "./views/pages/dashboard";
+import CreateNotePage from "./views/pages/notes/create";
+import LoginPage from "./views/pages/auth/login";
+import RegisterPage from "./views/pages/auth/register";
+import BookmarkPage from "./views/pages/bookmark";
 
 const routes = {
-  '/': DashboardPage, // default page
-  '/dashboard': DashboardPage, // default page
-  '/note/create': CreateNotePage,
+    "/": DashboardPage, // default page
+    "/dashboard": DashboardPage, // default page
+    "/note/create": CreateNotePage,
+    "/bookmark": BookmarkPage,
 
-  // Auth
-  '/login': LoginPage,
-  '/register': RegisterPage,
+    // Auth
+    "/login": LoginPage,
+    "/register": RegisterPage,
 };
 
 export default routes;
